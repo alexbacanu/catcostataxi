fcf08df33a0688a9ba1f31dae5eb9b544576dfe3
Alexandru Bacanu <alex.bacanu@live.com>
2023-01-11 14:35:11 +0200

    build: first commit, create next-app with tailwind

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Cat costa taxi | Cat costa Uber | Cat costa Bolt</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Aplicatie pentru verificarea pretului la un taxi, Uber, Bolt" />
+      <meta name="keywords" content="cat costa taxi, cat costa uber, cat costa bolt" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
