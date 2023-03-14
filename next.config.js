8ba6dfb9760910f3b5b8d02087694a337ad277f4
Alexandru Bacanu <alex.bacanu@live.com>
2023-03-14 20:00:03 +0200

    fix: revert csp change

--- next.config.js
+++ next.config.js
@@ -1,36 +1,8 @@
-// const ContentSecurityPolicy = `
-// default-src 'self';
-// script-src 'self' 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com js.hcaptcha.com;
-// style-src 'self' 'unsafe-inline';
-// img-src * blob: data:;
-// connect-src *;
-// font-src 'self';
-// `
-const csp = `
-script-src 'self' 'unsafe-eval' 'unsafe-inline' vitals.vercel-insights.com cdn.vercel-insights.com js.hcaptcha.com maps.googleapis.com;
-`
-
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: csp.replace(/\n/g, ""),
-  },
-]
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
     typedRoutes: true,
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ]
   },
 }
 
