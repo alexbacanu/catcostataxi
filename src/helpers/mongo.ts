738ef42c8c578e31153771f69b6b5cd7717bcf3b
Alexandru Bacanu <alex.bacanu@live.com>
2023-03-15 22:25:29 +0200

    feat: use cache per request for next 13

--- src/helpers/mongo.ts
+++ src/helpers/mongo.ts
@@ -1,5 +1,6 @@
 import clientPromise from "@/clients/mongoClient"
 import type { Condition, ObjectId } from "mongodb"
+import { cache } from "react"
 
 export type Route = {
   _id?: Condition<ObjectId>
@@ -27,7 +28,7 @@ export type Company = {
   dateAdded?: Date
   dateUpdated?: Date
 }
-export async function fetchCompaniesByLoc(city: string) {
+export const fetchCompaniesByLoc = cache(async (city: string) => {
   const client = await clientPromise
   const db = client.db(process.env.MONGO_DB ?? "")
   const companies = db.collection("companies")
@@ -39,9 +40,9 @@ export async function fetchCompaniesByLoc(city: string) {
   if (allCompanies.length == 0) console.warn(`😱 Warning: No taxi companies found in this city: ${city}`)
 
   return allCompanies
-}
+})
 
-export async function fetchRecentRoutes() {
+export const fetchRecentRoutes = cache(async () => {
   const client = await clientPromise
   const db = client.db(process.env.MONGO_DB ?? "")
   const routes = db.collection("routes")
@@ -55,9 +56,9 @@ export async function fetchRecentRoutes() {
   if (recentRoutes.length == 0) console.warn("😱 Warning: No recent routes found")
 
   return recentRoutes
-}
+})
 
-export async function fetchAllRoutesIds() {
+export const fetchAllRoutesIds = cache(async () => {
   const client = await clientPromise
   const db = client.db(process.env.MONGO_DB ?? "")
   const routes = db.collection("routes")
@@ -68,9 +69,9 @@ export async function fetchAllRoutesIds() {
   const allRoutesIds = allRoutes.map((object) => object.hash)
 
   return allRoutesIds
-}
+})
 
-export async function fetchSingleRoute(hash: string) {
+export const fetchSingleRoute = cache(async (hash: string) => {
   const client = await clientPromise
   const db = client.db(process.env.MONGO_DB ?? "")
   const routes = db.collection("routes")
@@ -79,26 +80,28 @@ export async function fetchSingleRoute(hash: string) {
   if (!singleRoute) console.warn(`😱 Warning: No route found with hash: ${hash}`)
 
   return singleRoute
-}
+})
 
-export async function submitRoute(
-  hash: string,
-  tripData: {
-    fromAddress: string
-    fromLoc?: string
-    toAddress: string
-    toLoc?: string
+export const submitRoute = cache(
+  async (
+    hash: string,
+    tripData: {
+      fromAddress: string
+      fromLoc?: string
+      toAddress: string
+      toLoc?: string
+    }
+  ) => {
+    const client = await clientPromise
+    const db = client.db(process.env.MONGO_DB ?? "")
+    const routes = db.collection("routes")
+
+    const requestedRoute = await routes.findOne<Route>({ hash }, { projection: { _id: 0, createdAt: 0 } })
+
+    if (!requestedRoute) {
+      await routes.insertOne({ hash, ...tripData, createdAt: new Date() })
+    }
+
+    return requestedRoute
   }
-) {
-  const client = await clientPromise
-  const db = client.db(process.env.MONGO_DB ?? "")
-  const routes = db.collection("routes")
-
-  const requestedRoute = await routes.findOne<Route>({ hash }, { projection: { _id: 0, createdAt: 0 } })
-
-  if (!requestedRoute) {
-    await routes.insertOne({ hash, ...tripData, createdAt: new Date() })
-  }
-
-  return requestedRoute
-}
+)
