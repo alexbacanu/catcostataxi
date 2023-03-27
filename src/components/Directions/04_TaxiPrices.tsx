9fa8ee2f3fec64cc41f225a103736f8fce5e69f8
Alexandru Bacanu <alex.bacanu@live.com>
2023-03-27 17:07:06 +0300

    style: update taxi prices style

--- src/components/Directions/04_TaxiPrices.tsx
+++ src/components/Directions/04_TaxiPrices.tsx
@@ -86,152 +86,100 @@ export default function TaxiPrices({ companies }: Props) {
       <div className="flex w-full flex-col justify-between gap-8 lg:flex-row">
         <div className="card-base grow">
           {/* --- */}
-          <div className="flex justify-between">
-            <div className="flex items-center">
-              <IconCurrencyDollar />
-              <span className="pl-2">Cursă</span>
-            </div>
-
-            <div className="flex flex-auto items-center justify-end">
-              <button
-                onClick={() => setModifyToggle(!modifyToggle)}
-                className="rounded-md bg-black/10 px-2 py-1 text-xs ring-1 ring-neutral-800/20 hover:bg-black/5 dark:bg-white/10 dark:ring-neutral-200/20 dark:hover:bg-white/5"
-              >
-                {modifyToggle ? "Salvează" : "Modifică"}
-              </button>
-            </div>
-
-            <div className="mx-4 my-1 w-[1px] grow-0 bg-black/10 dark:bg-white/10"></div>
-
-            <div className="flex items-center">
-              <IconSun className="h-4 w-4 font-bold text-amber-500 dark:font-semibold dark:text-amber-400" />
-              <Switch
-                checked={nightToggle}
-                onChange={setNightToggle}
-                className={`${
-                  nightToggle ? "bg-indigo-500" : "bg-black/10 hover:bg-black/5 dark:bg-white/10 dark:hover:bg-white/5"
-                } relative mx-1 inline-flex h-6 w-11 items-center rounded-full ring-1 ring-neutral-800/20 dark:ring-neutral-200/20`}
-              >
-                <span className="sr-only">Schimba pretul</span>
-                <span
-                  className={`${
-                    nightToggle ? "translate-x-6" : "translate-x-1"
-                  } inline-block h-4 w-4 rounded-full bg-white ring-1 ring-neutral-800/20 transition dark:ring-neutral-200/20`}
+          <div className="flex flex-col justify-between gap-x-4 sm:flex-row">
+            <div className="flex gap-x-4">
+              <div className="flex items-center">
+                <IconCurrencyDollar />
+                <span className="pl-2">Cursă</span>
+              </div>
+              <div className="flex grow font-bold text-amber-500 dark:font-semibold dark:text-amber-400">
+                <input
+                  value="Bucharest"
+                  type="text"
+                  className="w-full rounded-lg bg-black/5 px-2 ring-1 ring-neutral-800/20 dark:bg-white/10 dark:ring-neutral-200/20 dark:hover:bg-white/5"
                 />
-              </Switch>
-              <IconMoon className="h-4 w-4 text-indigo-500" />
+              </div>
+            </div>
+
+            <div className="flex justify-center gap-x-4 py-4 sm:py-0">
+              <div className="flex items-center justify-end">
+                <button
+                  onClick={() => setModifyToggle(!modifyToggle)}
+                  className="rounded-md bg-black/10 px-2 py-1 text-xs ring-1 ring-neutral-800/20 hover:bg-black/5 dark:bg-white/10 dark:ring-neutral-200/20 dark:hover:bg-white/5"
+                >
+                  {modifyToggle ? "Salvează" : "Modifică"}
+                </button>
+              </div>
+
+              <div className="my-1 w-[1px] bg-black/10 dark:bg-white/10"></div>
+
+              <div className="flex items-center">
+                <IconSun className="h-4 w-4 font-bold text-amber-500 dark:font-semibold dark:text-amber-400" />
+                <Switch
+                  checked={nightToggle}
+                  onChange={setNightToggle}
+                  className={`${
+                    nightToggle
+                      ? "bg-indigo-500"
+                      : "bg-black/10 hover:bg-black/5 dark:bg-white/10 dark:hover:bg-white/5"
+                  } relative mx-1 inline-flex h-6 w-11 items-center rounded-full ring-1 ring-neutral-800/20 dark:ring-neutral-200/20`}
+                >
+                  <span className="sr-only">Schimba pretul</span>
+                  <span
+                    className={`${
+                      nightToggle ? "translate-x-6" : "translate-x-1"
+                    } inline-block h-4 w-4 rounded-full bg-white ring-1 ring-neutral-800/20 transition dark:ring-neutral-200/20`}
+                  />
+                </Switch>
+                <IconMoon className="h-4 w-4 text-indigo-500" />
+              </div>
             </div>
           </div>
-          <div className="flex flex-col gap-x-8 gap-y-4 sm:flex-row">
-            {/* Standard */}
-            <div className="flex grow items-center justify-center gap-x-6">
-              {/* Image */}
-              <div className="relative flex shrink-0 items-center justify-center p-2">
-                <Image src="/taxi-yellow.png" alt="Standard taxi" width={569 * 0.18} height={361 * 0.18} />
+
+          <div className="flex w-full items-center gap-x-4 py-2">
+            {/* Image */}
+            <div className="shrink-0">
+              <div className="relative">
+                <Image src="/taxi-yellow.png" alt="Standard taxi" width={569 * 0.2} height={361 * 0.2} />
                 <div className="absolute bottom-0 rounded-md bg-black/40 px-2 py-1 text-center text-xs font-medium text-white shadow-lg backdrop-blur-[6px] dark:bg-white/20">
-                  Standard
+                  Taxi
                 </div>
               </div>
-
-              {/* Table */}
-              <table className="text-left">
-                <tbody>
-                  <tr>
-                    <th
-                      scope="row"
-                      className={`${
-                        nightToggle ? "text-indigo-500" : "text-amber-500 dark:text-amber-400"
-                      }  w-full text-right font-bold dark:font-semibold`}
-                    >
-                      {modifyToggle ? (
-                        <input
-                          value={nightToggle ? priceData.nightPrice : priceData.dayPrice}
-                          type="number"
-                          step={0.01}
-                          onChange={nightToggle ? handleChange("nightPrice") : handleChange("dayPrice")}
-                          className="w-full rounded-lg bg-black/5 p-1 px-2 text-right ring-1 ring-neutral-800/20 dark:bg-white/10 dark:ring-neutral-200/20 dark:hover:bg-white/5"
-                        />
-                      ) : (
-                        <div className="p-1 px-2 text-lg">
-                          {nightToggle ? priceData.nightPrice : priceData.dayPrice}
-                        </div>
-                      )}
-                    </th>
-
-                    <td>lei/km</td>
-                  </tr>
-
-                  <tr>
-                    <th
-                      className={`${
-                        nightToggle ? "text-indigo-500" : "text-amber-500 dark:text-amber-400"
-                      }  text-right font-bold dark:font-semibold`}
-                    >
-                      <span className="p-1 px-2 text-lg">
-                        {totalPrice(nightToggle ? "nightPrice" : "dayPrice").toFixed(2)}
-                      </span>
-                    </th>
-
-                    <td>lei/total</td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
 
-            {/* Premium */}
-            <div className="flex grow items-center justify-center gap-x-6">
-              {/* Image */}
-              <div className="relative flex shrink-0 items-center justify-center p-2">
-                <Image src="/taxi-black.png" alt="Premium taxi" width={569 * 0.18} height={361 * 0.18} />
-                <div className="absolute bottom-0 rounded-md bg-indigo-800/50 px-2 py-1 text-center text-xs font-medium text-white shadow-lg backdrop-blur-[6px] dark:bg-indigo-400/30">
-                  Premium
-                </div>
+            {/* Prices */}
+            <div className="grid grid-cols-2 items-center">
+              <div>Per km (lei):</div>
+              <div
+                className={`${
+                  nightToggle ? "text-indigo-500" : "text-amber-500 dark:text-amber-400"
+                } text-lg font-bold dark:font-semibold`}
+              >
+                {modifyToggle ? (
+                  <input
+                    value={nightToggle ? priceData.nightPrice : priceData.dayPrice}
+                    type="number"
+                    step={0.01}
+                    onChange={nightToggle ? handleChange("nightPrice") : handleChange("dayPrice")}
+                    className="w-full rounded-lg bg-black/5 px-2 ring-1 ring-neutral-800/20 dark:bg-white/10 dark:ring-neutral-200/20 dark:hover:bg-white/5"
+                  />
+                ) : (
+                  <div className="px-2">{nightToggle ? priceData.nightPrice : priceData.dayPrice}</div>
+                )}
               </div>
-
-              {/* Table */}
-              <table className="text-left">
-                <tbody>
-                  <tr>
-                    <th
-                      className={`${
-                        nightToggle ? "text-indigo-500" : "text-amber-500 dark:text-amber-400"
-                      }  w-full text-right font-bold dark:font-semibold`}
-                    >
-                      {modifyToggle ? (
-                        <input
-                          value={nightToggle ? priceData.nightPricePlus : priceData.dayPricePlus}
-                          type="number"
-                          step={0.01}
-                          onChange={nightToggle ? handleChange("nightPricePlus") : handleChange("dayPricePlus")}
-                          className="w-full rounded-lg bg-black/5 p-1 px-2 text-right ring-1 ring-neutral-800/20 dark:bg-white/10 dark:ring-neutral-200/20 dark:hover:bg-white/5"
-                        />
-                      ) : (
-                        <div className="p-1 px-2 text-lg">
-                          {nightToggle ? priceData.nightPricePlus : priceData.dayPricePlus}
-                        </div>
-                      )}
-                    </th>
-
-                    <td>lei/km</td>
-                  </tr>
-
-                  <tr>
-                    <th
-                      className={`${
-                        nightToggle ? "text-indigo-500" : "text-amber-500 dark:text-amber-400"
-                      } text-right font-bold dark:font-semibold`}
-                    >
-                      <span className="p-1 px-2 text-lg">
-                        {totalPrice(nightToggle ? "nightPricePlus" : "dayPricePlus").toFixed(2)}
-                      </span>
-                    </th>
-
-                    <td>lei/total</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div>Total (lei):</div>
+              <div
+                className={`${
+                  nightToggle ? "text-indigo-500" : "text-amber-500 dark:text-amber-400"
+                } px-2 text-lg font-bold dark:font-semibold`}
+              >
+                {totalPrice(nightToggle ? "nightPrice" : "dayPrice").toFixed(2)}
+              </div>
             </div>
+
+            {/* City selector */}
           </div>
+
           <p className="pt-2 text-xs italic">
             Rețineți că acestea sunt doar tarife estimative. Tarifele reale variază în funcție de trafic, vreme și alte
             condiții neprevazute. Taxele nu sunt afișate. Informațiile furnizate pe acest site sunt estimative.
@@ -239,7 +187,7 @@ export default function TaxiPrices({ companies }: Props) {
           {/* --- */}
         </div>
         <div className="flex min-w-[28%] flex-row gap-8 lg:flex-col">
-          <div className="card-base flex grow flex-col items-center justify-between gap-x-2 sm:flex-row sm:justify-center">
+          <div className="card-base flex grow flex-col items-center justify-center gap-x-2 sm:flex-row sm:justify-between">
             {/* --- */}
             <div className="flex items-center">
               <IconRoute2 />
@@ -250,7 +198,7 @@ export default function TaxiPrices({ companies }: Props) {
             </p>
             {/* --- */}
           </div>
-          <div className="card-base flex grow flex-col items-center justify-between gap-x-2 sm:flex-row sm:justify-center">
+          <div className="card-base flex grow flex-col items-center justify-center gap-x-2 sm:flex-row sm:justify-between">
             {/* --- */}
             <div className="flex items-center">
               {duration.value > duration_in_traffic.value ? <IconTrafficCone /> : <IconClockHour4 />}
