a54b2543065b89021032f80a308f5cf818daf3c1
Alexandru Bacanu <alex.bacanu@live.com>
2023-04-21 23:04:33 +0300

    fix: small dict update

--- app/[lang]/(legal)/privacy/history/page.tsx
+++ app/[lang]/(legal)/privacy/history/page.tsx
@@ -20,6 +20,7 @@ type Props = {
 export default async function PrivacyHistoryPage({ params }: Props) {
   const dictionary = await getDictionary(params.lang)
   const legal = await fetchLegal("privacy", params.lang)
+  const lang = params.lang === "ro" ? "ro-RO" : "en-GB"
 
   return (
     <section className="layout-mx flex-col items-start gap-y-4">
@@ -28,7 +29,7 @@ export default async function PrivacyHistoryPage({ params }: Props) {
         <a key={document.version} href={`/privacy/history/${document.version}`}>
           <ul className="flex gap-x-2 text-lg font-normal leading-4">
             <li>{dictionary.legal.modified}</li>
-            <li>{new Date(document.modified).toLocaleDateString()}</li>
+            <li>{new Date(document.modified).toLocaleDateString(lang)}</li>
             <li>&middot;</li>
             <li>{dictionary.legal.version}</li>
             <li>{document.version}</li>
