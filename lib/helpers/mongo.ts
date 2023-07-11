929eb824a56158bb6508fdccfc5195b99b0e458f
Alexandru Bacanu <alex.bacanu@live.com>
2023-07-12 01:31:58 +0300

    Quick fix building errors

--- lib/helpers/mongo.ts
+++ lib/helpers/mongo.ts
@@ -68,7 +68,9 @@ export const fetchSingleLegal = cache(async (type: string, lang: string, version
   return singleLegal;
 });
 
-export const fetchCompaniesByLoc = cache(async (city: string) => {
+export const fetchCompaniesByLoc = cache(async (city?: string) => {
+  if (!city) throw new Error("City is required");
+
   const client = await clientPromise;
   const db = client.db(process.env.MONGO_DB ?? "");
   const companies = db.collection("companies");
