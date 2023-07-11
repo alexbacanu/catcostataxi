aa260328b4f19ad2b9869e6c216bd9fb9aa697ad
Alexandru Bacanu <alex.bacanu@live.com>
2023-07-12 01:26:04 +0300

    No longer experimental

--- next.config.js
+++ next.config.js
@@ -14,9 +14,9 @@ const nextConfig = {
     ],
   },
   experimental: {
-    appDir: true,
     mdxRs: true,
   },
-}
-const withMDX = require("@next/mdx")()
-module.exports = withMDX(nextConfig)
+};
+
+const withMDX = require("@next/mdx")();
+module.exports = withMDX(nextConfig);
